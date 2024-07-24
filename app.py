deaaf3394c3a4a08c10f8a43e6b2fd8e8dd29a4e
enrcho <enrcho@naver.com>
2024-07-24 11:49:59 +0900

    수정

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import streamlit as st
+import random
 
 st.write("Hello Streamlit!!")
 st.markdown("Hello **Streamlit**")
 
 st.header('주사위 게임', divider='rainbow')
-st.button('주사위던지기', type='primary')
+clicked = st.button('주사위던지기', type='primary')
+if clicked:
+    n = random.randint(1,6)
+    st.image(f'./img/{n}.png')
+    
